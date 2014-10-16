a6f55c6c7646fa8d2155dc0309130de5bfe10eaa
Erik Sjöströmm <metallicgeek@gmail.com>
2014-10-16 23:20:08 +0200

    lol

--- mothership.py
+++ mothership.py
@@ -1,9 +1,11 @@
 from cockhammer import *
 from dator import *
 
-playernames = ["Cockhammer", "Dator", "Lol"]
-players = {"Cockhammer": cockhammer(),
-           "Dator": dator()}
+# g = lamdba: cockhammer()
+
+playernames = ["Cockhammer", "Dator"]
+players = {"Cockhammer": cockhammer,
+           "Dator": dator}
 
 
 def who_won(res1, res2):
@@ -28,16 +30,28 @@ def who_won(res1, res2):
         return "WTF!"
 
 
-# for i in range(len(playernames) - 1):
-#     print("Turn")
-#     player1 = playernames[i]
-#     print(player1)
-#     for j in range(i + 1, len(playernames)):
-#         player2 = playernames[j]
-#         print(player2)
+for i in range(len(playernames) - 1):
+    player1 = playernames[i]
+    for j in range(i + 1, len(playernames)):
+        wins = 0
+        losses = 0
+        draws = 0
+        stats = {"Wins": 0,
+                 "Losses": 0,
+                 "Draws": 0}
+        player2 = playernames[j]
+        for k in range(1000000):
+            result = who_won(players[player1](), players[player2]())
+            if result == "Win":
+                stats["Wins"] += 1
+            elif result == "Loss":
+                stats["Losses"] += 1
+            else:
+                stats["Draws"] += 1
+        print(stats)
 
-res1 = cockhammer()
-res2 = dator()
-print("Cockhammer: " + res1)
-print("Dator: " + res2)
-print(who_won(res1, res2))
+# res1 = cockhammer()
+# res2 = dator()
+# print("Cockhammer: " + res1)
+# print("Dator: " + res2)
+# print(who_won(res1, res2))
