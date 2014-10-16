caa8d48103209d8c77210d93df39e22fd084810a
Erik Sjöströmm <metallicgeek@gmail.com>
2014-10-16 22:30:41 +0200

    lol

--- mothership.py
+++ mothership.py
@@ -14,13 +14,13 @@ def who_won(res1, res2):
             return "Win"
         else:
             return "Loss"
-    elif res1 == "scissor":
+    elif res1 == "scissors":
         if res2 == "paper":
             return "Win"
         else:
             return "Loss"
     elif res1 == "rock":
-        if res2 == "scissor":
+        if res2 == "scissors":
             return "Win"
         else:
             return "Loss"
@@ -28,11 +28,16 @@ def who_won(res1, res2):
         return "WTF!"
 
 
-for i in range(len(playernames) - 1):
-    print("Turn")
-    player1 = playernames[i]
-    print(player1)
-    for j in range(i + 1, len(playernames)):
-        player2 = playernames[j]
-        print(player2)
+# for i in range(len(playernames) - 1):
+#     print("Turn")
+#     player1 = playernames[i]
+#     print(player1)
+#     for j in range(i + 1, len(playernames)):
+#         player2 = playernames[j]
+#         print(player2)
 
+res1 = cockhammer()
+res2 = dator()
+print("Cockhammer: " + res1)
+print("Dator: " + res2)
+print(who_won(res1, res2))
