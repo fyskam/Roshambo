1cfb6f96f50357369925040f9cea0b37f423d39b
Erik Sjöströmm <metallicgeek@gmail.com>
2014-10-16 22:24:52 +0200

    lol

--- mothership.py
+++ mothership.py
@@ -1,11 +1,38 @@
 from cockhammer import *
 from dator import *
 
+playernames = ["Cockhammer", "Dator", "Lol"]
 players = {"Cockhammer": cockhammer(),
            "Dator": dator()}
 
 
-for player1 in range(len(players) - 1):
+def who_won(res1, res2):
+    if res1 == res2:
+        return "Draw"
+    elif res1 == "paper":
+        if res2 == "rock":
+            return "Win"
+        else:
+            return "Loss"
+    elif res1 == "scissor":
+        if res2 == "paper":
+            return "Win"
+        else:
+            return "Loss"
+    elif res1 == "rock":
+        if res2 == "scissor":
+            return "Win"
+        else:
+            return "Loss"
+    else:
+        return "WTF!"
+
+
+for i in range(len(playernames) - 1):
+    print("Turn")
+    player1 = playernames[i]
     print(player1)
-    for player2 in range(1, len(player))
+    for j in range(i + 1, len(playernames)):
+        player2 = playernames[j]
         print(player2)
+
