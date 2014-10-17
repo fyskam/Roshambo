54fe8aae4e728276066d2f006690eb04b8e324f0
Erik Sjöströmm <metallicgeek@gmail.com>
2014-10-17 03:55:26 +0200

    lol

---
+++ dator2.py
@@ -0,0 +1,7 @@
+#Datorns's robot
+import random
+
+
+def dator2():
+    hands = ["rock", "paper", "scissors"]
+    return random.choice(hands)
--- mothership.py
+++ mothership.py
@@ -1,57 +1,82 @@
 from cockhammer import *
 from dator import *
+from dator2 import *
+import pickle
+import collections
 
-# g = lamdba: cockhammer()
 
-playernames = ["Cockhammer", "Dator"]
+playernames = ["Cockhammer", "Dator", "Dator2"]
 players = {"Cockhammer": cockhammer,
-           "Dator": dator}
+           "Dator": dator,
+           "Dator2": dator2}
+# high_scores = {"Cockhammer": 0,
+#                "Dator": 0,
+#                "Dator2": 0}
+# with open("highscore.pkl", "wb") as out:
+#     pickle.dump(high_scores, out)
 
 
 def who_won(res1, res2):
     if res1 == res2:
-        return "Draw"
+        return "d"
     elif res1 == "paper":
         if res2 == "rock":
-            return "Win"
+            return "1"
         else:
-            return "Loss"
+            return "2"
     elif res1 == "scissors":
         if res2 == "paper":
-            return "Win"
+            return "1"
         else:
-            return "Loss"
+            return "2"
     elif res1 == "rock":
         if res2 == "scissors":
-            return "Win"
+            return "1"
         else:
-            return "Loss"
+            return "2"
     else:
         return "WTF!"
 
 
+def highscore(score1, score2):
+    with open("highscore.pkl", "rb") as in_:
+        high_scores = pickle.load(in_)
+    if score1.name not in high_scores:
+        high_scores[score1.name] = 0
+    high_scores[score1.name] = high_scores[score1.name] + score1.score
+    if score2.name not in high_scores:
+        high_scores[score2.name] = 0
+    high_scores[score2.name] = high_scores[score2.name] + score2.score
+    with open("highscore.pkl", "wb") as out:
+        pickle.dump(high_scores, out)
+
+
 for i in range(len(playernames) - 1):
+    Score = collections.namedtuple("Score", ["name", "score"])
     player1 = playernames[i]
     for j in range(i + 1, len(playernames)):
+        player2 = playernames[j]
+        f = open('RPSdata.txt', 'r+')
+        f.seek(0, 2)
         wins = 0
         losses = 0
-        draws = 0
-        stats = {"Wins": 0,
-                 "Losses": 0,
-                 "Draws": 0}
-        player2 = playernames[j]
-        for k in range(1000000):
-            result = who_won(players[player1](), players[player2]())
-            if result == "Win":
-                stats["Wins"] += 1
-            elif result == "Loss":
-                stats["Losses"] += 1
-            else:
-                stats["Draws"] += 1
-        print(stats)
+        for k in range(100):
+            res1 = players[player1]()
+            res2 = players[player2]()
+            result = who_won(res1, res2)
+            if result == "1":
+                wins += 1
+            elif result == "2":
+                losses += 1
+            f.write(res1 + " " + res2 + " " + result + "\n")
+        player1_score = Score(player1, (wins - losses))
+        player2_score = Score(player2, player1_score.score * (-1))
+        highscore(player1_score, player2_score)
 
-# res1 = cockhammer()
-# res2 = dator()
-# print("Cockhammer: " + res1)
-# print("Dator: " + res2)
-# print(who_won(res1, res2))
+with open("highscore.pkl", "rb") as in_:
+    new_high_scores = pickle.load(in_)
+
+print("{{TITLE:^{PAGE_WIDTH}}}".format(PAGE_WIDTH=80).format(TITLE="HIGH SCORES"))
+print("-" * 80)
+for name, score in new_high_scores.items():
+    print("{{name:>{col_width}}} | {{score:<{col_width}}}".format(col_width=(80-3)//2).format(name=name, score=score))
